a00d392cc691226fd30fcebeda43e1465697e0ec
Anmol Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-04-26 22:17:12 +0530

    add table component

--- components/table/table.js
+++ components/table/table.js
@@ -1,62 +1,39 @@
 import styles from "./table.module.css";
-const Table = (props) => {
+
+const Table = ({ userData }) => {
   return (
     <table className={styles.table}>
       <thead>
         <tr>
+          <th>id</th>
           <th>First Name</th>
           <th>Last Name</th>
           <th>Email</th>
           <th>Gender</th>
-          <th>Phone</th>
-          <th>Car</th>
-          <th>Quote</th>
           <th>Income ($)</th>
           <th>City</th>
+          <th>Car</th>
+          <th>Quote</th>
+          <th>Phone Price</th>
         </tr>
       </thead>
       <tbody>
-        {props.userData &&
-          props.userData.map((user) => (
-            <tr key={user._id}>
-              <td>{user.fname}</td>
-              <td>{user.lname}</td>
-              <td>{user.email}</td>
-              <td>{user.gender}</td>
-              <td>{user.phone}</td>
-              <td>{user.car}</td>
-              <td>{user.quote}</td>
-              <td>{user.income}</td>
-              <td>{user.city}</td>
-            </tr>
-          ))}
+        {userData?.map((user) => (
+          <tr key={user.id}>
+            <td>{user.firstName}</td>
+            <td>{user.lastName}</td>
+            <td>{user.email}</td>
+            <td>{user.gender}</td>
+            <td>{user.income}</td>
+            <td>{user.city}</td>
+            <td>{user.car}</td>
+            <td>{user.quote}</td>
+            <td>{user.phone_price}</td>
+          </tr>
+        ))}
       </tbody>
     </table>
   );
 };
 
 export default Table;
-
-// const Table = ({ userData, fieldsToShow }) => {
-//   return (
-//     <table className={styles.table}>
-//       <thead>
-//         <tr>
-//           {fieldsToShow.map((field) => (
-//             <th key={field}>{field}</th>
-//           ))}
-//         </tr>
-//       </thead>
-//       <tbody>
-//         {userData &&
-//           userData.map((user) => (
-//             <tr key={user._id}>
-//               {fieldsToShow.map((field) => (
-//                 <td key={`${user._id}-${field}`}>{user[field]}</td>
-//               ))}
-//             </tr>
-//           ))}
-//       </tbody>
-//     </table>
-//   );
-// };
