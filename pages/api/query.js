8053e3fb49e42dd9b8f9029e734bfd202ce50b19
Anmol Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-04-26 19:33:10 +0530

    change logic according to fetchData

--- pages/api/query.js
+++ pages/api/query.js
@@ -6,76 +6,81 @@ const fetchData = async (url, results) => {
 };
 
 // Routes for fetching the required data
-export async function fetchQuery(req, res, results, url) {
+const fetchQuery = async (url, results) => {
   try {
-    const data = fetchData(url);
-    res.status(200).json(results);
+    const res = await fetch(`api/query/${url}`);
+    const data = await res.json();
+    // res.status(200).json(results);
+    return { data, results };
   } catch (error) {
     console.log(error);
     res.status(500).send("Error while fetching data in" + url);
   }
-}
-
-export const bmwMercedesIncome = async () => {
-  // we can also use: { $or: [ { car: "BMW", income: {$lt: "$5.00"} }, { car: "Mercedes-Benz", income: {$lt: "$5.00"} } ] }
-  let query = {
-    $and: [
-      { car: { $in: ["BMW", "Mercedes-Benz"] } },
-      { income: { $lt: "5" } },
-    ],
-  };
-  const results = await User.find(query);
-  const url = "bmwMercedesIncome";
-
-  return fetchQuery(results, url);
 };
 
-export const malePhonePrice = async () => {
-  let query = {
-    $and: [{ gender: "Male" }, { phone_price: { $gt: "10000" } }],
-  };
-  const results = await User.find(query);
-  const url = "malePhonePrice";
+export const queries = {
+  bmwMercedesIncome: async () => {
+    let query = {
+      $and: [
+        { car: { $in: ["BMW", "Mercedes-Benz"] } },
+        { income: { $lt: "5" } },
+      ],
+    };
+    const results = await User.find(query);
+    const url = "bmwMercedesIncome";
 
-  return fetchQuery(results, url);
-};
+    return fetchData(url, results);
+  },
 
-export const lastNameCharacterLength = async () => {
-  let query = {
-    lname: { $regex: "^M" },
-    quote: { $gt: 15 },
-    email: { $regex: ".*M.*$" },
-  };
-  const results = await User.find(query);
-  const url = "malePhonePrice";
+  malePhonePrice: async () => {
+    let query = {
+      $and: [{ gender: "Male" }, { phone_price: { $gt: "10000" } }],
+    };
+    const results = await User.find(query);
+    const url = "malePhonePrice";
 
-  return fetchQuery(results, url);
-};
+    return fetchData(url, results);
+  },
 
-export const bmwMercedesAudiEmailNoDigit = async () => {
-  let query = {
-    car: { $in: ["BMW", "Mercedes-Benz", "Audi"] },
-    email: { $not: { $regex: "\\d" } },
-  };
-  const results = await User.find(query);
-  const url = "malePhonePrice";
+  lastNameCharacterLength: async () => {
+    let query = {
+      lname: { $regex: "^M" },
+      quote: { $gt: 15 },
+      email: { $regex: ".*M.*$" },
+    };
+    const results = await User.find(query);
+    const url = "lastNameCharacterLength";
 
-  return fetchQuery(results, url);
-};
+    return fetchData(url, results);
+  },
 
-export const top10Cities = async () => {
-  const results = await User.aggregate([
-    {
-      $group: {
-        _id: "$city",
-        users_count: { $sum: 1 },
-        avg_income: { $avg: "$income" },
+  bmwMercedesAudiEmailNoDigit: async () => {
+    let query = {
+      car: { $in: ["BMW", "Mercedes-Benz", "Audi"] },
+      email: { $not: { $regex: "\\d" } },
+    };
+    const results = await User.find(query);
+    const url = "bmwMercedesAudiEmailNoDigit";
+
+    return fetchData(url, results);
+  },
+
+  top10Cities: async () => {
+    const results = await User.aggregate([
+      {
+        $group: {
+          _id: "$city",
+          users_count: { $sum: 1 },
+          avg_income: { $avg: "$income" },
+        },
       },
-    },
-    { $sort: { users_count: -1 } },
-    { $limit: 10 },
-  ]);
-  const url = "malePhonePrice";
+      { $sort: { users_count: -1 } },
+      { $limit: 10 },
+    ]);
+    const url = "top10Cities";
 
-  return fetchQuery(results, url);
+    return fetchData(url, results);
+  },
 };
+
+export default fetchQuery;
