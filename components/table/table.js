d98c8dc30c9a6d2fba1e28e6dbe58ce2b42db92e
Anmol Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-04-27 19:23:22 +0530

    update table component with styling
    remove rendering of empty table fields if 5th option is used

--- components/table/table.js
+++ components/table/table.js
@@ -3,7 +3,7 @@ import styles from "./table.module.css";
 const Table = ({ userData, apiSelection }) => {
   return (
     <>
-      {userData.length > 0 && apiSelection === "5" && (
+      {apiSelection === "5" && (
         <table className={styles.table}>
           <thead>
             <tr>
@@ -25,38 +25,40 @@ const Table = ({ userData, apiSelection }) => {
           </tbody>
         </table>
       )}
-      <table className={styles.table}>
-        <thead>
-          <tr className={styles.tablerow}>
-            <th className={styles.tableheading}>id</th>
-            <th className={styles.tableheading}>First Name</th>
-            <th className={styles.tableheading}>Last Name</th>
-            <th className={styles.tableheading}>Email</th>
-            <th className={styles.tableheading}>Gender</th>
-            <th className={styles.tableheading}>Income ($)</th>
-            <th className={styles.tableheading}>City</th>
-            <th className={styles.tableheading}>Car</th>
-            <th className={styles.tableheading}>Quote</th>
-            <th className={styles.tableheading}>Phone Price</th>
-          </tr>
-        </thead>
-        <tbody>
-          {userData?.map((user) => (
-            <tr key={user.id} className={styles.tablerow}>
-              <td className={styles.tablecell}>{user.id}</td>
-              <td className={styles.tablecell}>{user.first_name}</td>
-              <td className={styles.tablecell}>{user.last_name}</td>
-              <td className={styles.tablecell}>{user.email}</td>
-              <td className={styles.tablecell}>{user.gender}</td>
-              <td className={styles.tablecell}>{user.income}</td>
-              <td className={styles.tablecell}>{user.city}</td>
-              <td className={styles.tablecell}>{user.car}</td>
-              <td className={styles.tablecell}>{user.quote}</td>
-              <td className={styles.tablecell}>{user.phone_price}</td>
+      {apiSelection !== "5" && (
+        <table className={styles.table}>
+          <thead>
+            <tr>
+              <th className={styles.tableheading}>id</th>
+              <th className={styles.tableheading}>First Name</th>
+              <th className={styles.tableheading}>Last Name</th>
+              <th className={styles.tableheading}>Email</th>
+              <th className={styles.tableheading}>Gender</th>
+              <th className={styles.tableheading}>Income ($)</th>
+              <th className={styles.tableheading}>City</th>
+              <th className={styles.tableheading}>Car</th>
+              <th className={styles.tableheading}>Quote</th>
+              <th className={styles.tableheading}>Phone Price</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {userData?.map((user) => (
+              <tr key={user.id} className={styles.tablerow}>
+                <td className={styles.tablecell}>{user.id}</td>
+                <td className={styles.tablecell}>{user.first_name}</td>
+                <td className={styles.tablecell}>{user.last_name}</td>
+                <td className={styles.tablecell}>{user.email}</td>
+                <td className={styles.tablecell}>{user.gender}</td>
+                <td className={styles.tablecell}>{user.income}</td>
+                <td className={styles.tablecell}>{user.city}</td>
+                <td className={styles.tablecell}>{user.car}</td>
+                <td className={styles.tablecell}>{user.quote}</td>
+                <td className={styles.tablecell}>{user.phone_price}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </>
   );
 };
