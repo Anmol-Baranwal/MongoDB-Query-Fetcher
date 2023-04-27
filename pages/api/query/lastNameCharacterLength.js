2d26ec8b1cae20f953a9f402a06e846cb0a8079e
Anmol Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-04-27 11:23:15 +0530

    update working query with proper testing

--- pages/api/query/lastNameCharacterLength.js
+++ pages/api/query/lastNameCharacterLength.js
@@ -2,13 +2,48 @@ import User from "@/models/userModel";
 
 const lastNameCharacterLength = async (req, res) => {
   try {
-    let query = {
-      lname: { $regex: "^M" },
-      quote: { $gt: 15 },
-      email: { $regex: ".*M.*$" },
-    };
-
-    const results = await User.find(query);
+    const results = await User.aggregate([
+      {
+        $addFields: {
+          last_name_regex: {
+            $regexMatch: {
+              input: "$email",
+              regex: {
+                $concat: [
+                  ".*",
+                  {
+                    $toLower: "$last_name",
+                  },
+                  ".*@.*",
+                ],
+              },
+              options: "i",
+            },
+          },
+        },
+      },
+      {
+        $match: {
+          last_name: {
+            $regex: "^M",
+          },
+          $expr: {
+            $lt: [
+              {
+                $strLenCP: "$quote",
+              },
+              40,
+            ],
+          },
+          last_name_regex: true,
+        },
+      },
+      {
+        $project: {
+          last_name_regex: 0,
+        },
+      },
+    ]);
     res.status(200).json(results);
   } catch (error) {
     console.log(error);
